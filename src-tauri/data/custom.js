719f3073d696ff2a38682e37008398064abde710
4gz7wk62rd-pixel <4gz7wk62rd@privaterelay.appleid.com>
2026-04-19 08:26:28 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -1,15 +1,99 @@
-console.log('custom.js ----')
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});// core
+const {
+    addPluginListener,
+    invoke,
+    Channel,
+    checkPermissions,
+    convertFileSrc,
+    isTauri,
+    PluginListener,
+    requestPermissions,
+    Resource,
+    transformCallback,
+    SERIALIZE_TO_IPC_FN,
+} = window.__TAURI__.core
 
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded')
-})
+// app
+const {
+    defaultWindowIcon,
+    fetchDataStoreIdentifiers,
+    getIdentifier,
+    getName,
+    getTauriVersion,
+    getVersion,
+    hide,
+    removeDataStore,
+    setDockVisibility,
+    setTheme,
+    show,
+} = window.__TAURI__.app
 
-// 监听全屏变化
-document.addEventListener('fullscreenchange', () => {
-    console.log('fullscreenchange')
-})
+// event
+const { emit, emitTo, listen, once } = window.__TAURI__.event
 
-// Safari / WebKit
-document.addEventListener('webkitfullscreenchange', () => {
-    console.log('webkitfullscreenchange')
-})
+// dpi
+const {
+    LogicalPosition,
+    LogicalSize,
+    PhysicalPosition,
+    PhysicalSize,
+    Position,
+    Size,
+} = window.__TAURI__.dpi
+
+// image
+const { Image, transformImage } = window.__TAURI__.image
+
+// menu
+const {
+    CheckMenuItem,
+    IconMenuItem,
+    itemFromKind,
+    Menu,
+    MenuItem,
+    NativeIcon,
+    PredefinedMenuItem,
+    Submenu,
+} = window.__TAURI__.menu
+
+// path
+const {
+    appDataDir,
+    appConfigDir,
+    appLocalDataDir,
+    appCacheDir,
+    appLogDir,
+    audioDir,
+    cacheDir,
+    configDir,
+    dataDir,
+    desktopDir,
+    documentDir,
+    downloadDir,
+    executableDir,
+    fontDir,
+    homeDir,
+    pictureDir,
+    publicDir,
+    resourceDir,
+    runtimeDir,
+    templateDir,
+    videoDir,
+    sep,
+    delimiter,
+    basename,
+    dirname,
+    extname,
+    join,
+    normalize,
+    resolve,
+    isAbsolute,
+    localDataDir,
+    resolveResource,
+    tempDir,
+} = window.__TAURI__.path
+
+// 调用新建窗口等函数
+const { WebviewWindow } = window.__TAURI__.webviewWindow
+
+// 等等接口，请参考Tauri2官方文档：https://v2.tauri.app/reference/javascript/api/#vanilla-js-api
